08e04ee69383e19f286bff11cfde4f52aa96ec40
CompileError <gngn15401@gmail.com>
2022-01-13 23:03:31 +0800

    adjust typography

--- src/CourseFactory.java
+++ src/CourseFactory.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class CourseFactory {
 	Course getCourse(String info){
 		String[] s = info.split(" ");
-		if(Integer.parseInt(s[0]) == 1) {
+		if (Integer.parseInt(s[0]) == 1) {
 			return new Course(true,
 					(Integer.parseInt(s[2])==1?true:false),
 					Integer.parseInt(s[3]),
@@ -11,14 +11,13 @@ public class CourseFactory {
 					Integer.parseInt(s[4]),
 					Integer.parseInt(s[5]),
 					Integer.parseInt(s[6]));
-		}
-		else {
+		} else {
 			Random rd = new Random();
 			int week, start;
 			do {
-			week = rd.nextInt(5) + 1;
-			start = rd.nextInt(8) + 1;
-			}while(week == 2 && start == 8);
+				week = rd.nextInt(5) + 1;
+				start = rd.nextInt(8) + 1;
+			} while(week == 2 && start == 8);
 			return new Course(false,
 					(Integer.parseInt(s[2])==1?true:false),
 					Integer.parseInt(s[3]),
